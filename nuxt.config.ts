b461b1ea993c196e75874166abb480414307e71e
yuyinws <lyc657508366@gmail.com>
2023-06-09 23:01:54 +0800

    chore: update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,9 +43,7 @@ export default defineNuxtConfig({
     head: {
       viewport: 'width=device-width,initial-scale=1',
       link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
